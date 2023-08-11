db912d44f584da3046f6a134cba2c64590e63b57
pinomaker-hoo <inhoo23@naver.com>
2023-08-11 17:12:31 +0900

    init : clone project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
+// ** Nest Imports
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+
+// ** Custom Module Imports
 import { AppModule } from './app.module';
 
+// ** Swagger Config Imports
+import swaggerConfig from './config/swaggerConfig';
+
+// ** Logger Config Imports
+import LoggerService from './util/logger/logger.service';
+
+// ** Express Imports
+import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AllExceptionsFilter } from './filter/ExceptionFilter';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(LoggerService));
+  app.useGlobalFilters(new AllExceptionsFilter());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+  app.use('/file', express.static('./uploads'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'src/views'));
+  app.setViewEngine('hbs');
+  swaggerConfig(app);
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
