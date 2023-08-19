73c8ee4d1826292f0fb8ae402a6d9df61a1c6646
pinomaker-hoo <inhoo23@naver.com>
2023-08-19 14:31:10 +0900

    feat : Add 주석

--- src/main.ts
+++ src/main.ts
@@ -18,18 +18,35 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AllExceptionsFilter } from './filter/ExceptionFilter';
 
 async function bootstrap() {
+  // ** Server Container 생성
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     bufferLogs: true,
   });
+
+  // ** Logger
   app.useLogger(app.get(LoggerService));
+
+  // ** FIlter 개념
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  // ** Global Pipe Line
   app.useGlobalPipes(new ValidationPipe());
+
+  // ** Cors Setting
   app.enableCors();
+
+  // ** Static Rouing
   app.use('/file', express.static('./uploads'));
+
+  // ** View Setting
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'src/views'));
   app.setViewEngine('hbs');
+
+  // ** Swagger Setting
   swaggerConfig(app);
+
+  // ** Server ON Handler
   await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
