87ccb5e238de048c79519b1f3d10b6fe8cf0534f
rlawnaks <rlawnaks1234@naver.com>
2023-08-23 13:02:54 +0900

    feat : add Phone Service

---
+++ src/api/phone/service/phone.service.ts
@@ -0,0 +1,114 @@
+// ** Nest Imports
+import { Injectable } from '@nestjs/common';
+import PhoneRepository from '../repository/phone.repository';
+
+// ** enum, dto, entity, types Imports
+import CommonResponse from 'src/common/dto/api.response';
+import RequestPhoneSaveDto from '../dto/phone.save.dto';
+import { BadRequestException } from 'src/exception/customException';
+
+@Injectable()
+export default class PhoneService {
+  constructor(private readonly phoneRepository: PhoneRepository) {}
+
+  public async savePhone(
+    dto: RequestPhoneSaveDto,
+  ): Promise<CommonResponse<any>> {
+    const findPhone = await this.phoneRepository.findOne({
+      where: { number: dto.phone },
+    });
+
+    if (findPhone) {
+      throw new BadRequestException('Exist Phone Number');
+    }
+
+    await this.phoneRepository.save(
+      this.phoneRepository.create({
+        name: dto.name,
+        number: dto.phone,
+      }),
+    );
+
+    return CommonResponse.of({
+      statusCode: 200,
+      message: '전화번호를 생성합니다.',
+    });
+  }
+
+  public async findPhone(
+    id: number
+  ): Promise<CommonResponse<any>> {
+    
+    const findPhone = await this.phoneRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!findPhone) {
+      throw new BadRequestException('NOT Exist Phone Number');
+    }
+
+    return CommonResponse.of({
+      statusCode: 200,
+      message: '전화번호를 조회했습니다.',
+      data: findPhone,
+    });
+
+  }
+
+  public async findAll(): Promise<CommonResponse<any>> {
+
+    const findPhoneList = await this.phoneRepository.find();
+
+    return CommonResponse.of({
+      statusCode: 200,
+      message: '전화번호를 전체 조회했습니다.',
+      data: findPhoneList,
+    });
+  }
+
+  public async updatePhone(
+    id: number,
+    dto: RequestPhoneSaveDto
+  ): Promise<CommonResponse<any>> {
+
+    const findPhone = await this.phoneRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!findPhone) {
+      throw new BadRequestException('NOT Exist Phone Number');
+    }
+
+    findPhone.name = dto.name
+    findPhone.number = dto.phone
+
+    this.phoneRepository.save(findPhone)
+
+    return CommonResponse.of({
+      statusCode: 200,
+      message: '전화번호를 수정합니다.',
+    });
+
+  }
+
+  public async deletePhone(
+    id: number
+  ): Promise<CommonResponse<any>> {
+
+    const findPhone = await this.phoneRepository.findOne({
+      where: { id: id },
+    });
+
+    if (!findPhone) {
+      throw new BadRequestException('NOT Exist Phone Number');
+    }
+
+    this.phoneRepository.delete(id)
+
+    return CommonResponse.of({
+      statusCode: 200,
+      message: '전화번호를 삭제합니다.',
+    });
+  }
+
+}
