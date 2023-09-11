ba21dfd8db29db99ce21ede0e8f3d194a9259f41
rlawnaks <105625545+rlawnaks@users.noreply.github.com>
2023-09-11 12:24:57 +0900

    Add : queryBuilder FindAllByUser And FindOneByUser

--- src/api/phone/repository/phone.repository.ts
+++ src/api/phone/repository/phone.repository.ts
@@ -4,6 +4,36 @@ import { Repository } from 'typeorm';
 // ** Custom Module Imports
 import { CustomRepository } from 'src/repository/typeorm-ex.decorator';
 import Phone from '../domain/phone.entity';
+import RequestPhoneFindDto from '../dto/phone.find.dto';
 
 @CustomRepository(Phone)
-export default class PhoneRepository extends Repository<Phone> {}
+export default class PhoneRepository extends Repository<Phone> {
+
+    public async findOneByUser(id: number, userId: number) {
+        return this.createQueryBuilder('phone')
+            .select([
+                'phone.id', 
+                'phone.name', 
+                'phone.number', 
+                'phone.userId',
+            ])
+            .where('phone.id = :id', {id})
+            .andWhere('phone.userId = :userId', {userId})
+            .getOne();
+    }
+
+    public async findAllByUser(userId: number) {
+        const qb = this.createQueryBuilder('phone')
+            .select([
+                'phone.id', 
+                'phone.name', 
+                'phone.number', 
+                'phone.userId',
+            ])
+            .where('phone.userId = :userId', {userId})
+
+            return qb.getManyAndCount();
+    }
+
+
+}
