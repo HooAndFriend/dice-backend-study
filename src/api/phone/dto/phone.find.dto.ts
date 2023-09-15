2fe17138b62fd5595aed9d1a3a6d70db3f40d734
rlawnaks <105625545+rlawnaks@users.noreply.github.com>
2023-09-15 14:50:37 +0900

    Add : type to ApiProperty

--- src/api/phone/dto/phone.find.dto.ts
+++ src/api/phone/dto/phone.find.dto.ts
@@ -1,17 +1,15 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString } from "class-validator";
-import RequestPagingDto from "src/common/dto/paging.dto";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
+import RequestPagingDto from 'src/common/dto/paging.dto';
 
 export default class RequestPhoneFindDto extends RequestPagingDto {
+  @ApiProperty({ example: '01057027017', type: 'string', required: false })
+  @IsOptional()
+  @IsString()
+  number: string;
 
-    @ApiProperty({example: '01057027017', required: false})
-    @IsOptional()
-    @IsString()
-    number: string;
-
-    @ApiProperty({example: '김주만', required: false})
-    @IsOptional()
-    @IsString()
-    name: string;
-
+  @ApiProperty({ example: '김주만', type: 'string', required: false })
+  @IsOptional()
+  @IsString()
+  name: string;
 }
